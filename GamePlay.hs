414f5053de90845d81bd7491bbb5b23664de2b76
Robert <rlamacraft@gmail.com>
2018-08-26 12:45:30 +0100

    Simplify functions that operate on the Move type, now contains mark as well as x and y co-ords of move

--- GamePlay.hs
+++ GamePlay.hs
@@ -2,18 +2,18 @@ module GamePlay () where
 
 import GameState
 
-type Move = (Int, Int)
+data Move = Move Mark Int Int
 
 data MoveResult = Success GameState
                 | AlreadyPlayed Int Int Mark deriving Show
 
-makeMove :: GameState -> Mark -> Move -> MoveResult
-makeMove (State state) mark (i, j) = case state !! i !! j of
+makeMove :: GameState -> Move -> MoveResult
+makeMove (State state) (Move mark i j) = case state !! i !! j of
   Played x -> AlreadyPlayed i j x
-  Unplayed -> Success (updateState (State state) mark (i, j))
+  Unplayed -> Success (updateState (State state) (Move mark i j))
 
-updateState :: GameState -> Mark -> Move -> GameState
-updateState (State state) mark (i, j) = State (before ++ [row] ++ after) where
+updateState :: GameState -> Move -> GameState
+updateState (State state) (Move mark i j) = State (before ++ [row] ++ after) where
   before = take i state
   after = drop (i + 1) state
   row = updateRow (state !! i) mark j
